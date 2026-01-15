882d110a2f6d49fdbe10b4137c36e33733fa0bce
Roy Sand <roy@sanddata.no>
2026-01-15 15:17:51 +0100

    Early start of Infrastructure project

---
+++ src/no.sanddata.ams.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace no.sanddata.ams.Application.Abstractions.Email;
+
+public interface IEmailService
+{
+    Task SendAsync(Domain.Users.Email recipient, string subject, string body);
+}
---
+++ src/no.sanddata.ams.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,11 @@
+﻿using no.sanddata.ams.Application.Abstractions.Email;
+
+namespace no.sanddata.ams.Infrastructure.Email;
+
+internal sealed class EmailService : IEmailService
+{
+    public Task SendAsync(Domain.Users.Email recipient, string subject, string body)
+    {
+        return Task.CompletedTask;
+    }
+}
