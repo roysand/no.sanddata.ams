9c350cdbe7f6f0e317eff8dd7f90e9c75d921c5d
Roy Sand <roy@sanddata.no>
2026-01-15 14:11:51 +0100

    Added CreateUserValidator

---
+++ src/no.sanddata.ams.Application/Users/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using no.sanddata.ams.Domain.Users;
+
+namespace no.sanddata.ams.Application.Users.CreateUser;
+
+public class CreateUserValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(v => v.Email)
+            .NotEmpty().WithMessage("Email address is required")
+            .EmailAddress().WithMessage("Your email address is not valid");
+        
+        RuleFor(v => v.FirstName)
+            .NotEmpty().WithMessage("First name is required");
+
+        RuleFor(v => v.LastName)
+            .NotEmpty().WithMessage("Last name is required");
+        
+        RuleFor(v => v.Password)
+            .NotEmpty().WithMessage("Password is required");
+    }
+}
