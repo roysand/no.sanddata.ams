91783cdeeab2efbb0ca6a240c9af4cd65c8b27ad
Roy Sand <roy@sanddata.no>
2026-01-15 17:37:14 +0100

    API is up an running! No other functionality than WheaterForecast yet!

---
+++ src/no.sanddata.ams.Api/Features/WeatherForecast/GetWeatherForecastEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using FastEndpoints;
+
+namespace no.sanddata.ams.Api.Features.WeatherForecast;
+
+public sealed class GetWeatherForecastEndpoint : EndpointWithoutRequest<WeatherForecastResponse[]>
+{
+    public override void Configure()
+    {
+        Get("/weatherforecast");
+        AllowAnonymous();
+    }
+
+#pragma warning disable CA5394 // Do not use insecure randomness
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        string[] summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        ];
+
+        WeatherForecastResponse[] forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecastResponse(
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            )).ToArray();
+
+        await Send.OkAsync(forecast, ct);
+    }
+#pragma warning restore CA5394
+}
+
+public sealed record WeatherForecastResponse(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
--- src/no.sanddata.ams.Api/Program.cs
+++ src/no.sanddata.ams.Api/Program.cs
@@ -1,41 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using FastEndpoints;
+using FastEndpoints.Swagger;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+builder.Services.AddFastEndpoints();
+builder.Services.SwaggerDocument();
 
-// Configure the HTTP request pipeline.
+WebApplication app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwaggerGen();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseFastEndpoints();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+await app.RunAsync().ConfigureAwait(false);
